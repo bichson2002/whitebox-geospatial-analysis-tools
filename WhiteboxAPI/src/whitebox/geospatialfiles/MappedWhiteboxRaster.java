d3df3528fa0fac4bae2c736be5708ceec3b846f0
kevin.a.green <kevin.a.green@gmail.com>
2013-04-08 16:09:27 +0000

    * Moved IDWinterWork synchronized block into a finally so that the progress bar is updated even if the file fails. * MappedWhiteboxRaster can now support any size of file. Internally it contains a List of MappedByteBuffers. * isReflectedAtEdges variable was moved to WhiteboxRasterBase and support was added to MappedWhiteboxRaster.

--- WhiteboxAPI/src/whitebox/geospatialfiles/MappedWhiteboxRaster.java
+++ WhiteboxAPI/src/whitebox/geospatialfiles/MappedWhiteboxRaster.java
@@ -9,6 +9,8 @@ import java.nio.FloatBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Implementation of WhiteboxRasterInterface that uses a memory mapped file for
@@ -17,7 +19,9 @@ import java.nio.channels.FileChannel.MapMode;
  */
 public class MappedWhiteboxRaster extends WhiteboxRasterBase implements WhiteboxRasterInterface {   
     
-    private MappedByteBuffer buffer;
+    private List<MappedByteBuffer> buffers;
+    
+    private static final int MAX_BUFFER_SIZE = 16777216; // 2^24 
     
     public MappedWhiteboxRaster(String headerFile, String fileAccess) {
         this(headerFile, fileAccess, false);
@@ -31,10 +35,15 @@ public class MappedWhiteboxRaster extends WhiteboxRasterBase implements Whitebox
         readHeaderFile();
         
         File file = new File(dataFile);
-        if (overwrite || !file.exists()) {
-            buffer = createNewDataFile(fileAccess);
-        } else {
-            buffer = openDataFile(fileAccess);
+        try {
+            if (overwrite || !file.exists()) {
+                buffers = createNewDataFile(fileAccess);
+            } else {
+                buffers = openDataFile(fileAccess);
+            }
+        } catch (IOException e) {
+            // Log and ignore?
+            System.out.println(e);
         }
         
         // Need to check if synchronous, if it is fileAccess should be "rwd" for direct
@@ -49,18 +58,18 @@ public class MappedWhiteboxRaster extends WhiteboxRasterBase implements Whitebox
         setFileAccess(fileAccess);
         
         File file = new File(dataFile);
-        if (!file.exists()) {
-            buffer = createNewDataFile(fileAccess);
-        } else {
-            buffer = openDataFile(fileAccess);
-        }
-        
+        try {
+            if (!file.exists()) {
+                buffers = createNewDataFile(fileAccess);
+            } else {
+                buffers = openDataFile(fileAccess);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }        
         setPropertiesUsingAnotherRaster(baseRasterHeader, dataType);
         
-        
         this.initialValue = initialValue;
-        
-        
         
     }
     
@@ -88,65 +97,103 @@ public class MappedWhiteboxRaster extends WhiteboxRasterBase implements Whitebox
         
         this.initialValue = initialValue;
         setFileAccess("rw");
-    }
-    
-    private MappedByteBuffer openDataFile(String fileAccess) {
+        
         try {
-            RandomAccessFile raf = new RandomAccessFile(this.dataFile, fileAccess);
-            MappedByteBuffer buf = raf.getChannel().map(MapMode.READ_WRITE, 0, raf.length());
-            buf.position(0);
-            buf.order(byteOrder);
-            return buf;
+            buffers = createNewDataFile(dataFile);
         } catch (IOException e) {
             System.out.println(e);
         }
-        
-        return null;
     }
     
-    private MappedByteBuffer createNewDataFile(String fileAccess) {
+    private List<MappedByteBuffer> openDataFile(String fileAccess) throws IOException {
+            
+        List<MappedByteBuffer> buffers = new ArrayList<>();
+
+        RandomAccessFile raf = new RandomAccessFile(this.dataFile, fileAccess);
+
+        // Get MapMode based on the provided fileAccess string
+        MapMode mapMode = (fileAccess.contains("w") ? MapMode.READ_WRITE : MapMode.READ_ONLY);
+
+        long startPos = 0;
+        long size;
         
-        try {
-            RandomAccessFile raf = new RandomAccessFile(this.dataFile, fileAccess);
-
-            // Although the size can't be > max int, use a long to prevent overflow error
-            long numCells = numberColumns * numberRows;
-            
-            MappedByteBuffer buf = raf.getChannel().map(MapMode.READ_WRITE, 0, numCells * cellSizeInBytes);
+        while (startPos < raf.length()) {
+            size = Math.min(raf.length() - startPos, MAX_BUFFER_SIZE);
+            MappedByteBuffer buf = raf.getChannel().map(mapMode, startPos, size);
             buf.order(byteOrder);
-
-            /*
-            if (getDataType() == DataType.FLOAT) {
-                byte[] defaultRow = new byte[numberColumns * cellSizeInBytes];
-                
-                //while (buf.position() < buf.limit()) {
-                //    buf.put(defaultRow);
-                //}
-            }*/
-            
             buf.position(0);
-            
-            return buf;
-            
-        } catch (IOException e) {
-            System.out.println(e);
+            startPos = startPos + size;
+            buffers.add(buf);
         }
         
-        return null;
+        return buffers;
+
+    }
+    
+    private List<MappedByteBuffer> createNewDataFile(String fileAccess) throws IOException {
+        
+        List<MappedByteBuffer> buffers = new ArrayList<>();
+
+        RandomAccessFile raf = new RandomAccessFile(this.dataFile, fileAccess);
+
+        // Get MapMode based on the provided fileAccess string
+        MapMode mapMode = (fileAccess.contains("w") ? MapMode.READ_WRITE : MapMode.READ_ONLY);
+        
+        long fileSize = (numberColumns * numberRows) * cellSizeInBytes;
+        long startPos = 0;
+        long size;
+        
+        while (startPos < fileSize) {
+            size = Math.min(fileSize - startPos, MAX_BUFFER_SIZE);
+            MappedByteBuffer buf = raf.getChannel().map(mapMode, startPos, size);
+            buf.order(byteOrder);
+            buf.position(0);
+            startPos = startPos + size;
+            buffers.add(buf);
+        }
+        
+        return buffers;
+        
     }
 
     @Override
     public void close() {
-        buffer.force(); // Flush any changes from memory to disk
-        buffer = null; // There is no way to force a mapping to close
+        //buffer.force(); // Flush any changes from memory to disk
+        //buffer = null; // There is no way to force a mapping to close
     }
 
     @Override
     public double getValue(int row, int column) {
-        int cellNum = row * numberColumns + column;
         
+        // Re-calculate row and column if out of bounds and file is reflected
+        if (row < 0 || row > numberRows || column < 0 || column > numberColumns) {
+            if (!isReflectedAtEdges) { return noDataValue; }
+
+            // if you get to this point, it is reflected at the edges
+            if (row < 0) { row = -row - 1; }
+            if (row >= numberRows) { row = numberRows - (row - numberRows) - 1; }
+            if (column < 0) { column = -column - 1; }
+            if (column >= numberColumns) { column = numberColumns - (column - numberColumns) - 1; }
+            // Check if the value is still out of bounds
+            if (row < 0 || row > numberRows || column < 0 || column > numberColumns) {
+                // it was too off grid to be reflected.
+                return noDataValue;
+            }
+        }
+
+        // Get the cell position in the file
+        int cellPos = (row * numberColumns + column) * cellSizeInBytes;
+        
+        // Get the correct buffer
+        int bufIndex = cellPos / MAX_BUFFER_SIZE;
+        MappedByteBuffer buffer = buffers.get(bufIndex);
+        
+        int bufPos = cellPos - (bufIndex * MAX_BUFFER_SIZE);
+        
+        buffer.position(bufPos);
+            
         try {
-            buffer.position(cellNum * cellSizeInBytes);
+            
         } catch (IllegalArgumentException e) {
             // The cell number is outside the bounds of this buffer
             return noDataValue;
@@ -168,20 +215,27 @@ public class MappedWhiteboxRaster extends WhiteboxRasterBase implements Whitebox
 
     @Override
     public void setValue(int row, int column, double value) {
-        int cellNum = row * numberColumns + column;
+        // Get the cell position in the file
+        int cellPos = (row * numberColumns + column) * cellSizeInBytes;
+        
+        // Get the correct buffer
+        int bufIndex = cellPos / MAX_BUFFER_SIZE;
+        MappedByteBuffer buffer = buffers.get(bufIndex);
+        
+        int bufPos = cellPos - (bufIndex * MAX_BUFFER_SIZE);
 
         switch (getDataType()) {
             case BYTE:
-                buffer.put(cellNum * cellSizeInBytes, (byte)value);
+                buffer.put(bufPos, (byte)value);
                 break;
             case DOUBLE:
-                buffer.putDouble(cellNum * cellSizeInBytes, value);
+                buffer.putDouble(bufPos, value);
                 break;
             case FLOAT:
-                buffer.putFloat(cellNum * cellSizeInBytes, (float)value);
+                buffer.putFloat(bufPos, (float)value);
                 break;
             case INTEGER:
-                buffer.putInt(cellNum * cellSizeInBytes, (int)value);
+                buffer.putInt(bufPos, (int)value);
                 break;
         }
     }
@@ -189,11 +243,38 @@ public class MappedWhiteboxRaster extends WhiteboxRasterBase implements Whitebox
     @Override
     public void setRowValues(int row, double[] vals) {
         
-        int firstCellNum = row * numberColumns;
+        // Get the first val position for the row
+        int cellPos = (row * numberColumns) * cellSizeInBytes;
         
-        buffer.position(firstCellNum);
+        // Get the correct buffer
+        int bufIndex = cellPos / MAX_BUFFER_SIZE;
+        MappedByteBuffer buffer = buffers.get(bufIndex);
         
-        //buffer.putDouble(vals);
+        int bufPos = cellPos - (bufIndex * MAX_BUFFER_SIZE);
+
+        switch (getDataType()) {
+            case BYTE:
+                for (double value : vals) {
+                    buffer.put(bufPos, (byte)value);
+                }
+                break;
+            case DOUBLE:
+                for (double value : vals) {
+                    buffer.putDouble(bufPos, value);
+                }
+                break;
+            case FLOAT:
+                for (double value : vals) {
+                    buffer.putFloat(bufPos, (float)value);
+                }
+                
+                break;
+            case INTEGER:
+                for (double value : vals) {
+                    buffer.putInt(bufPos, (int)value);
+                }
+                break;
+        }
     }
     
 }
