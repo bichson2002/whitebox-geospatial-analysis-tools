d8d11617cfaa3bb01c6c35a069cb6602e2e1b265
dr.john.lindsay@gmail.com <dr.john.lindsay@gmail.com@16469b0c-4758-3eea-7ec9-4212bf7c695f>
2013-06-04 16:56:19 +0000

    updating WhiteboxGIS with new build script

---
+++ branches/Roshani3/WhiteboxGIS/src/whiteboxgis/user_interfaces/FeatureSelectionPanel.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright (C) 2013 Dr. John Lindsay <jlindsay@uoguelph.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package whiteboxgis.user_interfaces;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.io.File;
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import whitebox.geospatialfiles.ShapeFile;
+import whitebox.geospatialfiles.VectorLayerInfo;
+import whitebox.geospatialfiles.shapefile.attributes.DBFField;
+import whitebox.geospatialfiles.shapefile.attributes.AttributeTable;
+
+/**
+ * This class is used to report the attributes of a selected feature from a
+ * displayed vector file.
+ *
+ * @author johnlindsay
+ */
+public class FeatureSelectionPanel extends JPanel implements PropertyChangeListener {
+
+    private ShapeFile shape = null;
+    private JTable table = new JTable();
+    private int selectedFeature = -1;
+    private VectorLayerInfo vli = null;
+    private DBFField[] fields;
+    
+    public FeatureSelectionPanel() {
+        createGui();
+    }
+
+    public VectorLayerInfo getVectorLayerInfo() {
+        return vli;
+    }
+
+    public void setVectorLayerInfo(VectorLayerInfo vli) {
+        this.vli = vli;
+        this.setShapeFileName(vli.getFileName());
+        this.vli.pcs.addPropertyChangeListener("selectedFeatureNumber", this);
+        createGui();
+    }
+
+    public String getShapeFileName() {
+        if (this.shape != null) {
+            return this.shape.getFileName();
+        } else {
+            return "not specified";
+        }
+    }
+
+    public void setShapeFileName(String shapeFileName) {
+        try {
+            this.shape = new ShapeFile(shapeFileName);
+        } catch (IOException ioe) {
+            
+        }
+        createGui();
+    }
+
+    public int getSelectedFeatureNumber() {
+        return selectedFeature;
+    }
+
+    public void setSelectedFeatureNumber(int selectedFeature) {
+        this.selectedFeature = selectedFeature;
+//        getDataTable();
+        updateTable();
+//        createGui();
+    }
+
+    private void createGui() {
+        try {
+            this.removeAll();
+            table = new JTable();
+            selectedFeature = -1;
+            Box mainBox = Box.createVerticalBox();
+            mainBox.add(Box.createVerticalStrut(10));
+
+            Box headerBox = Box.createHorizontalBox();
+            //headerBox.add(Box.createHorizontalStrut(10));
+            JLabel label = new JLabel("Selected Feature Attributes:");
+            Font newLabelFont=new Font(label.getFont().getName(),Font.BOLD,label.getFont().getSize());
+            label.setFont(newLabelFont);
+            headerBox.add(label);
+            headerBox.add(Box.createHorizontalGlue());
+            mainBox.add(headerBox);
+            mainBox.add(Box.createVerticalStrut(2));
+            Box scrollBox = Box.createHorizontalBox();
+            //scrollBox.add(Box.createHorizontalStrut(10));
+            setBackground(Color.WHITE);
+            getDataTable();
+            JScrollPane scroll = new JScrollPane(table);
+            int tableWidth = 170;
+            int numRows = 20;
+            if (table != null) {
+                tableWidth = table.getPreferredSize().width;
+                numRows = Math.min(20, table.getRowCount());
+            }
+            scroll.setPreferredSize(
+                    new Dimension(tableWidth + 5, table.getRowHeight() * (numRows + 1) + 5));
+
+            scrollBox.add(scroll);
+            scrollBox.add(Box.createHorizontalGlue());
+            mainBox.add(scrollBox);
+            this.add(mainBox, BorderLayout.WEST);
+            this.validate();
+            this.repaint();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private boolean noDatabaseAvailable = false;
+    private JTable getDataTable() {
+        try {
+            if (shape == null) {
+                return null;
+            }
+
+            int numColumns = 2;
+            File dbfFile = new File(shape.getDatabaseFile());
+            if (!dbfFile.exists()) {
+                noDatabaseAvailable = true;
+                return null;
+            }
+            shape.refreshAttributeTable();
+            AttributeTable attributeTable = shape.getAttributeTable();
+            int numRows = shape.getAttributeTable().getFieldCount() + 1;
+            //String[] ch = shape.attributeTable.getAttributeTableFieldNames();
+            fields = attributeTable.getAllFields();
+            String[] columnHeaders = {"Attribute", "Value"};
+            Object[][] data = new Object[numRows][numColumns];
+            data[0][0] = "REC #";
+            if (selectedFeature >= 0) {
+                Object[] rowData = attributeTable.getRecord(selectedFeature);
+                for (int a = 0; a < numRows - 1; a++) {
+                    data[a + 1][0] = fields[a].getName(); //ch[a];
+                    data[a + 1][1] = String.valueOf(rowData[a]);
+                }
+            } else {
+                for (int a = 0; a < numRows - 1; a++) {
+                    data[a + 1][0] = fields[a].getName(); //ch[a];
+                    data[a + 1][1] = "";
+                }
+            }
+            DefaultTableModel tm = new DefaultTableModel(data, columnHeaders);
+            
+            table = new JTable(tm) {
+                @Override
+                public Component prepareRenderer(TableCellRenderer renderer, int Index_row, int Index_col) {
+                    Component comp = super.prepareRenderer(renderer, Index_row, Index_col);
+                    //even index, selected or not selected
+                    if (Index_row % 2 == 0) {// && !isCellSelected(Index_row, Index_col)) {
+                        comp.setBackground(Color.WHITE);
+                        comp.setForeground(Color.BLACK);
+                    } else {
+                        comp.setBackground(new Color(225, 245, 255)); //new Color(210, 230, 255));
+                        comp.setForeground(Color.BLACK);
+                    }
+                    if (isCellSelected(Index_row, Index_col)) {
+                        comp.setForeground(Color.RED);
+                    }
+                    return comp;
+                }
+            };
+
+            table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+            TableColumn column = null;
+            for (int i = 0; i < numColumns; i++) {
+                column = table.getColumnModel().getColumn(i);
+                if (i == 0) {
+                    column.setPreferredWidth(80);
+                } else {
+                    column.setPreferredWidth(100);
+                }
+
+            }
+            return table;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void updateTable() {
+        try {
+            if (table == null || noDatabaseAvailable) {
+                return;
+            }
+            shape.refreshAttributeTable();
+            if (fields.length != shape.getAttributeTable().getFieldCount()) {
+                int oldSelectedFeature = selectedFeature;
+                createGui();
+                selectedFeature = oldSelectedFeature;
+            }
+            DefaultTableModel tm = (DefaultTableModel)table.getModel();
+            if (selectedFeature >= 0) {
+                tm.setValueAt(selectedFeature, 0, 1);
+                AttributeTable attributeTable = shape.getAttributeTable();
+                Object[] rowData = attributeTable.getRecord(selectedFeature - 1);
+                if (rowData == null) {
+                    return;
+                }
+                for (int a = 0; a < rowData.length; a++) {
+                    //tm.setValueAt(rowData[a], a, 1);
+                    if (fields[a].getDataType() == DBFField.DBFDataType.NUMERIC 
+                            && !(rowData[a] == null)) {
+                        tm.setValueAt((double)(rowData[a]), a + 1, 1);
+                    } else if (fields[a].getDataType() == DBFField.DBFDataType.FLOAT
+                            && !(rowData[a] == null)) {
+                        tm.setValueAt((float)(rowData[a]), a + 1, 1);
+                    } else {
+                        tm.setValueAt(String.valueOf(rowData[a]), a + 1, 1);
+                    }
+                    //tm.fireTableCellUpdated(a, 2);
+                }
+            } else {
+                tm.setValueAt("", 0, 1);
+                for (int a = 0; a < fields.length; a++) {
+                    tm.setValueAt("", a + 1, 1);
+                }
+            }
+            //tm.fireTableDataChanged();
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("selectedFeatureNumber")) {
+            setSelectedFeatureNumber((int) (evt.getNewValue()));
+        }
+    }
+}
