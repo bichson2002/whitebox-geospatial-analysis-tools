758117da6b1cafc0f9830631771530af89383920
ehsan_ro <ehsan_ro@yahoo.com>
2013-10-08 14:28:52 +0000

    Optimization Engine

--- LidarTools/src/plugins/SensorOptimizer.java
+++ LidarTools/src/plugins/SensorOptimizer.java
@@ -10,116 +10,125 @@ package plugins;
  */
 
 import jmetal.core.Algorithm;
+import jmetal.core.Operator;
 import jmetal.core.Problem;
 import jmetal.core.SolutionSet;
+import jmetal.operators.crossover.CrossoverFactory;
+import jmetal.operators.mutation.MutationFactory;
+import jmetal.operators.selection.SelectionFactory;
+import jmetal.problems.ProblemFactory;
 import jmetal.qualityIndicator.QualityIndicator;
 import jmetal.util.Configuration;
 import jmetal.util.JMException;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
-import static jmetal.experiments.Main.fileHandler_;
-import static jmetal.experiments.Main.logger_;
-import jmetal.experiments.Settings;
-import jmetal.experiments.SettingsFactory;
+import jmetal.metaheuristics.nsgaII.NSGAII;
+import static jmetal.metaheuristics.nsgaII.NSGAII_main.fileHandler_;
+import static jmetal.metaheuristics.nsgaII.NSGAII_main.logger_;
+import jmetal.problems.Roshani;
+
 
 
 public class SensorOptimizer {
     public static Logger      logger_ ;      // Logger object
-  public static FileHandler fileHandler_ ; // FileHandler object
-/**
-   * @param args Command line arguments.
-   * @throws JMException
-   * @throws IOException
-   * @throws SecurityException
-   * Usage: three options
-   *      - jmetal.experiments.Main algorithmName
-   *      - jmetal.experiments.Main algorithmName problemName
-   *      - jmetal.experiments.Main algorithmName problemName paretoFrontFile
-   * @throws ClassNotFoundException 
-   */
-  public static void main(String [] args) throws
-                                  JMException, SecurityException, IOException, 
-                                  IllegalArgumentException, IllegalAccessException, 
+    public static FileHandler fileHandler_ ; // FileHandler object
+  
+     public static void main(String [] args) throws 
+                                  JMException, 
+                                  SecurityException, 
+                                  IOException, 
                                   ClassNotFoundException {
-    Problem   problem   ;         // The problem to solve
-    Algorithm algorithm ;         // The algorithm to use
-
+    Problem   problem   ; // The problem to solve
+    Algorithm algorithm ; // The algorithm to use
+    Operator  crossover ; // Crossover operator
+    Operator  mutation  ; // Mutation operator
+    Operator  selection ; // Selection operator
+    
+    HashMap  parameters ; // Operator parameters
+    
     QualityIndicator indicators ; // Object to get quality indicators
-
-    Settings settings = null;
-
-    String algorithmName   = "" ;
-    String problemName     = "Kursawe" ; // Default problem
-    String paretoFrontFile = "" ;
-
-    indicators = null ;
-    problem = null ;
-
-    if (args.length == 0) { //
-      System.err.println("Sintax error. Usage:") ;
-      System.err.println("a) jmetal.experiments.Main algorithmName ") ;
-      System.err.println("b) jmetal.experiments.Main algorithmName problemName") ;
-      System.err.println("c) jmetal.experiments.Main algorithmName problemName paretoFrontFile") ;
-      System.exit(-1) ;
-    } // if
-    else if (args.length == 1) { // algorithmName
-      algorithmName = args[0] ;
-      Object [] settingsParams = {problemName} ;
-      settings = (new SettingsFactory()).getSettingsObject(algorithmName, settingsParams) ;
-      } // if
-    else if (args.length == 2) { // algorithmName problemName
-      algorithmName = args[0] ;
-      problemName = args[1] ;
-      Object [] settingsParams = {problemName} ;
-      settings = (new SettingsFactory()).getSettingsObject(algorithmName, settingsParams) ;
-      } // if
-    else if (args.length == 3) { // algorithmName problemName paretoFrontFile
-      algorithmName = args[0] ;
-      problemName = args[1] ;
-      paretoFrontFile = args[2] ;
-      Object [] settingsParams = {problemName} ;
-      settings = (new SettingsFactory()).getSettingsObject(algorithmName, settingsParams) ;
-    } // if
 
     // Logger object and file to store log messages
     logger_      = Configuration.logger_ ;
-    fileHandler_ = new FileHandler(algorithmName + ".log");
+    fileHandler_ = new FileHandler("NSGAII_main.log"); 
     logger_.addHandler(fileHandler_) ;
+        
+    indicators = null ;
+    if (args.length == 1) {
+      Object [] params = {"Real"};
+      problem = (new ProblemFactory()).getProblem(args[0],params);
+    } // if
+    else if (args.length == 2) {
+      Object [] params = {"Real"};
+      problem = (new ProblemFactory()).getProblem(args[0],params);
+      indicators = new QualityIndicator(problem, args[1]) ;
+    } // if
+    else { // Default problem
+        problem = new SensorOptimizerProblem("Real", 10);
+      //problem = new Roshani("Real");
+      //problem = new Kursawe("BinaryReal", 3);
+      //problem = new Water("Real");
+      //problem = new ZDT1("ArrayReal", 100);
+      //problem = new ConstrEx("Real");
+      //problem = new DTLZ1("Real");
+      //problem = new OKA2("Real") ;
+    } // else
+    
+    algorithm = new NSGAII(problem);
+    //algorithm = new ssNSGAII(problem);
 
-    algorithm = settings.configure();
+    // Algorithm parameters
+    algorithm.setInputParameter("populationSize",100);
+    algorithm.setInputParameter("maxEvaluations",25000);
 
-    if (args.length == 3) {
-    	Problem p = algorithm.getProblem() ;
-      indicators = new QualityIndicator(p, paretoFrontFile);
-    }
+    // Mutation and Crossover for Real codification 
+    parameters = new HashMap() ;
+    parameters.put("probability", 0.9) ;
+    parameters.put("distributionIndex", 20.0) ;
+    crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover", parameters);                   
+
+    parameters = new HashMap() ;
+    parameters.put("probability", 1.0/problem.getNumberOfVariables()) ;
+    parameters.put("distributionIndex", 20.0) ;
+    mutation = MutationFactory.getMutationOperator("PolynomialMutation", parameters);                    
+
+    // Selection Operator 
+    parameters = null ;
+    selection = SelectionFactory.getSelectionOperator("BinaryTournament2", parameters) ;                           
+
+    // Add the operators to the algorithm
+    algorithm.addOperator("crossover",crossover);
+    algorithm.addOperator("mutation",mutation);
+    algorithm.addOperator("selection",selection);
+
+    // Add the indicator object to the algorithm
+    algorithm.setInputParameter("indicators", indicators) ;
     
     // Execute the Algorithm
     long initTime = System.currentTimeMillis();
     SolutionSet population = algorithm.execute();
     long estimatedTime = System.currentTimeMillis() - initTime;
-
-    // Result messages
+    
+    // Result messages 
     logger_.info("Total execution time: "+estimatedTime + "ms");
+    logger_.info("Variables values have been writen to file VAR");
+    population.printVariablesToFile("VAR");    
     logger_.info("Objectives values have been writen to file FUN");
     population.printObjectivesToFile("FUN");
-    logger_.info("Variables values have been writen to file VAR");
-    population.printVariablesToFile("VAR");
-
+  
     if (indicators != null) {
       logger_.info("Quality indicators") ;
       logger_.info("Hypervolume: " + indicators.getHypervolume(population)) ;
       logger_.info("GD         : " + indicators.getGD(population)) ;
       logger_.info("IGD        : " + indicators.getIGD(population)) ;
       logger_.info("Spread     : " + indicators.getSpread(population)) ;
-      logger_.info("Epsilon    : " + indicators.getEpsilon(population)) ;
-
-      if (algorithm.getOutputParameter("evaluations") != null) {
-        Integer evals = (Integer)algorithm.getOutputParameter("evaluations") ;
-        int evaluations = (Integer)evals.intValue();
-        logger_.info("Speed      : " + evaluations + " evaluations") ;
-      } // if
+      logger_.info("Epsilon    : " + indicators.getEpsilon(population)) ;  
+     
+      int evaluations = ((Integer)algorithm.getOutputParameter("evaluations")).intValue();
+      logger_.info("Speed      : " + evaluations + " evaluations") ;      
     } // if
   } //main
 }
