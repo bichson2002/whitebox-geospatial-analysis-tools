be0aa2ceb9be43d91e4e566fc3ef881d17d7b8b4
ehsan_ro@yahoo.com <ehsan_ro@yahoo.com@16469b0c-4758-3eea-7ec9-4212bf7c695f>
2013-11-06 14:53:01 +0000

    Optimizing Kriging based on its variance

--- branches/Roshani/LidarTools/src/plugins/SensorVarianceOptimizerProblem.java
+++ branches/Roshani/LidarTools/src/plugins/SensorVarianceOptimizerProblem.java
@@ -8,6 +8,7 @@ package plugins;
  *
  * @author Dr. Ehsan Roshani
  */
+//import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,20 +27,23 @@ import whitebox.geospatialfiles.WhiteboxRaster;
 import whitebox.geospatialfiles.shapefile.attributes.DBFException;
 
 
-public class SensorOptimizerProblem extends Problem{
+public class SensorVarianceOptimizerProblem extends Problem{
     // defining the lower and upper limits
   public static  WhiteboxRaster Image;
-  public static final double [] LOWERLIMIT = {587993, 5474193};
-  public static final double [] UPPERLIMIT = {601703, 5546399};           
-  double difMin = 100000000;
-  
+  public static final double [] LOWERLIMIT = {588907, 5475107};
+  public static final double [] UPPERLIMIT = {600789, 5545485};           
+  double difMin = 1000000000;
+  Kriging k;
    /**
   * Constructor.
   * Creates a default instance of the Water problem.
   * @param solutionType The solution type must "Real" or "BinaryReal".
   */
-  public SensorOptimizerProblem(String solutionType, int NV, WhiteboxRaster img) {
-    Image = img;
+  public SensorVarianceOptimizerProblem(String solutionType, int NV, WhiteboxRaster img) {
+    
+      
+      Image = img;
+    
     //reads all the target data
     double[][] targetData = new double[img.getNumberRows()][img.getNumberColumns()]; 
       for (int r = 0; r < img.getNumberRows(); r++) {
@@ -50,7 +54,7 @@ public class SensorOptimizerProblem extends Problem{
       
     numberOfVariables_   = NV *2 ;
     numberOfObjectives_  = 2 ;
-    numberOfConstraints_ = 0 ;
+    numberOfConstraints_ = 1 ;
     problemName_         = "SensorOptimizer";
 	        
     upperLimit_ = new double[numberOfVariables_];
@@ -83,6 +87,7 @@ public class SensorOptimizerProblem extends Problem{
    */
   @Override
   public void evaluate(Solution solution) throws JMException {  
+      k = null;
       List<KrigingPoint> pnts = new ArrayList<>();
       
       for (int i = 0; i < solution.getDecisionVariables().length; i=i+2) {
@@ -93,7 +98,7 @@ public class SensorOptimizerProblem extends Problem{
           pnts.add(pp);
       }
       
-      Kriging k = new Kriging();
+      k = new Kriging();
       
       
       k.Points = pnts;
@@ -101,6 +106,9 @@ public class SensorOptimizerProblem extends Problem{
       k.LagSize = 2000;
       k.Anisotropic = false;
       Kriging.Variogram var = k.SemiVariogram(Kriging.SemiVariogramType.Spherical, 1, 35,false);
+      
+     
+      
       k.resolution = 914;
       k.BMinX = 588907;
       k.BMaxX = 600789;
@@ -111,38 +119,49 @@ public class SensorOptimizerProblem extends Problem{
       outPnts = k.InterpolatePoints(var, outPnts, 5);
       //k.BuildRaster("G:\\Optimized Sensory Network\\PALS\\20120607\\test"+".dep", outPnts);
       
-      double dif = 0;
+//      double dif = 0;
+//      for (int i = 0; i < outPnts.size(); i++) {
+//          int r = ((i+1)-((i+1)%Image.getNumberColumns()))/ Image.getNumberColumns();
+//          int c = (i%Image.getNumberColumns());
+//          
+//          dif += Math.abs(outPnts.get(i).z-Image.getValue(r, c));
+//      }
+//      
+      
+      double sDif = 0;
       for (int i = 0; i < outPnts.size(); i++) {
-          int r = ((i+1)-((i+1)%Image.getNumberColumns()))/ Image.getNumberColumns();
-          int c = (i%Image.getNumberColumns());
-          
-          dif += Math.abs(outPnts.get(i).z-Image.getValue(r, c));
+          sDif += (outPnts.get(i).v);
       }
       
-      
+      sDif = Math.abs(sDif);
     double [] f = new double[2] ; // 5 functions
     
     
-      if (difMin>= dif) {
-          difMin = dif;
+      if (difMin>= sDif) {
+          difMin = sDif;
+//          for (int i = 0; i < outPnts.size(); i++) {
+//              System.out.print(i);
+//                      
+//          }
           k.BuildRaster("G:\\Optimized Sensory Network\\PALS\\20120607\\test"+".dep", outPnts,false);
+          k.BuildRaster("G:\\Optimized Sensory Network\\PALS\\20120607\\testVar"+".dep", outPnts,true);
           try {
               k.DrawShapeFile("G:\\Optimized Sensory Network\\PALS\\20120607\\test.shp", pnts);
           } catch (DBFException ex) {
-              Logger.getLogger(SensorOptimizerProblem.class.getName()).log(Level.SEVERE, null, ex);
+              Logger.getLogger(SensorVarianceOptimizerProblem.class.getName()).log(Level.SEVERE, null, ex);
           } catch (IOException ex) {
-              Logger.getLogger(SensorOptimizerProblem.class.getName()).log(Level.SEVERE, null, ex);
+              Logger.getLogger(SensorVarianceOptimizerProblem.class.getName()).log(Level.SEVERE, null, ex);
           }
       }
     
     // First function
-    f[0] = dif ;
+    f[0] = sDif ;
     // Second function
-    f[1] = dif ;
+    f[1] = sDif ;
              
     solution.setObjective(0,f[0]);    
     solution.setObjective(1,f[1]);
-    System.out.println(dif);
+    System.out.println(sDif+" : "+ difMin);
   } // evaluate
 
   /** 
@@ -151,25 +170,20 @@ public class SensorOptimizerProblem extends Problem{
    * @throws JMException 
    */  
   public void evaluateConstraints(Solution solution) throws JMException {
+      
+      
+      
     double [] constraint = new double[1]; // 7 constraints
-    double [] x          = new double[2]; // 3 objectives
-        
-    x[0] = solution.getDecisionVariables()[0].getValue();
-    x[1] = solution.getDecisionVariables()[1].getValue();
- 
-    constraint[0] = 1 - (0.00139/(x[0]*x[1])+4.94*x[1]-0.08)             ;
     
-    double total = 0.0;
-    int number = 0;
-    for (int i = 0; i < numberOfConstraints_; i++) {
-      if (constraint[i]<0.0){
-        total+=constraint[i];
-        number++;
-      } // int
-    } // for
-        
-    solution.setOverallConstraintViolation(total);    
-    solution.setNumberOfViolatedConstraint(number);        
+      for (int i = 0; i < k.Pairs.size(); i++) {
+          if (k.Pairs.get(i).Distance<= k.resolution) {
+              constraint[0]+=(k.resolution - k.Pairs.get(i).Distance);
+          }
+      }
+ 
+    solution.setOverallConstraintViolation(constraint[0]);    
+    solution.setNumberOfViolatedConstraint(1);  
+    System.out.println(constraint[0]);
   } // evaluateConstraints   
     
 }
